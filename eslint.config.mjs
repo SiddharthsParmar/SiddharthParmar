15c25c8047a18825bbec56197739ac96e9b4185d
Siddharth <siddharthakk3704@gmail.com>
2025-04-26 09:07:11 +0530

    eslint disable

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,13 +1,20 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
-
+import { defineConfig } from "eslint/config";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
+export default defineConfig([
+	{
+		linterOptions: {
+			reportUnusedInlineConfigs: "error",
+		},
+	},
+]);
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,18 +1,18 @@
 
 "use client"
 
-import { useState } from "react"
+
 
 
 
 const Navbar = () => {
-  const [theme , setTheme] = useState(true);
+
    
   return (
     <div className="h-14 w-full  flex justify-between" id="navbar-container">
 <div className="flex justify-between items-center gap-3.5">
     <h2 className="text-gray-600">siddharth.official.swe@gmail.com</h2>
-    <a href="" className={theme==true?"bg-white px-4.5 rounded-4xl border-gray-600 border-1":"bg-black text-white px-4.5 rounded-4xl border-gray-600 border-1"}>CV</a>
+    <a href="" className="bg-white px-4.5 rounded-4xl border-gray-600 border-1">CV</a>
 </div>
 <div className="flex items-center ">
     <a href="">Github</a><span>/</span><a href="">Linkedin</a>
